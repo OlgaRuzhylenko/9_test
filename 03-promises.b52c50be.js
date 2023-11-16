68758829820d6889b7ab2f9ca1e0c4497810c612
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-11-16 17:11:54 +0000

    Deploying to gh-pages from @ OlgaRuzhylenko/9_test@58dab0655d58e7658dc086b76e5d285d2d28d8b3 🚀

--- 03-promises.b52c50be.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input[name='delay']"),document.querySelector("input[name='step']"),document.querySelector("input[name='amount']"),document.querySelector("button");
-//# sourceMappingURL=03-promises.b52c50be.js.map
