58dab0655d58e7658dc086b76e5d285d2d28d8b3
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-11-16 19:11:34 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+const form = document.querySelector('form')
+const firstDelayInput = document.querySelector("input[name='delay']");
+const delayStepInput = document.querySelector("input[name='step']");
+const amountInput = document.querySelector("input[name='amount']");
+const submitBtn = document.querySelector('button')
+
+const amountOfPromises = parseInt(amountInput.value);
+const firstDelay = parseInt(firstDelayInput.value);
+const step = parseInt(delayStepInput.value);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -7,9 +17,21 @@ function createPromise(position, delay) {
   }
 }
 
-const firstDelayInput = document.querySelector("input[name='delay']");
-const delayStepInput = document.querySelector("input[name='step']");
-const amountInput = document.querySelector("input[name='amount']");
-const submitBtn = document.querySelector('button')
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(evt) {
+  evt.preventDefault();
 
+    for (let i = 0; i < amountOfPromises; i+=1) {
+    const position = i + 1;
+    const delay = firstDelay + i * step;
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+  };
+ 
 
